28928ec4388678bb387d5aa0d18737c23350eda0
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-04-02 18:39:17 +0530

    feat: implement database connection and error handling in app setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import { connectDB } from './config/dbConfig';
+import { errorHandler } from './middlewares/error-middleware';
 
 const app = express();
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
+
+app.use(errorHandler);
 
 export default app;
