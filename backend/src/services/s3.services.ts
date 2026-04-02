2e7f6f7e1f1a555e23c13729415ef69d43284c88
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-04-02 19:46:03 +0530

    feat: enhance S3 upload function with content type and logging

--- backend/src/services/s3.services.ts
+++ backend/src/services/s3.services.ts
@@ -1,4 +1,6 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { logger } from '../utils/logger';
+import { AppError } from '../utils/global-error-handler';
 
 const s3 = new S3Client({
 	region: process.env.AWS_REGION,
@@ -8,14 +10,25 @@ const s3 = new S3Client({
 	},
 });
 
-export const uploadToS3 = async (file: Buffer, key: string) => {
-	const params = new PutObjectCommand({
-		Bucket: process.env.AWS_S3_BUCKET_NAME!,
-		Key: key,
-		Body: file,
-	});
+export const uploadToS3 = async (file: Buffer, key: string, contentType: string) => {
+	try {
+		const params = new PutObjectCommand({
+			Bucket: process.env.S3_BUCKET_NAME!,
+			Key: key,
+			Body: file,
+			ContentType: contentType,
+		});
 
-	await s3.send(params);
+		await s3.send(params);
 
-	return `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+		logger.info(`File uploaded successfully to S3 with key: ${key}`);
+
+		return `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+	} catch (error: any) {
+		logger.error(`Error uploading file to S3`, {
+			error: error.message,
+			key,
+		});
+		throw new AppError('Failed to upload file to S3', 500);
+	}
 };
