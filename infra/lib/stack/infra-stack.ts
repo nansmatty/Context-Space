8c42a38ac4aa74f418c76d5f7fd898c57054afd4
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-04-04 19:52:13 +0530

    feat: implement S3 bucket and Lambda integration in ContextSpaceStack

--- infra/lib/stack/infra-stack.ts
+++ infra/lib/stack/infra-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { S3BucketConstruct } from '../service-constructs/s3-bucket-construct';
+import { LambdaConstructs } from '../service-constructs/lambda-constructs';
+import { EventType } from 'aws-cdk-lib/aws-s3';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 
 export class ContextSpaceStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		// The code that defines your stack goes here
+		const s3Bucket = new S3BucketConstruct(this, 'S3BucketConstruct');
+		const lambdaConstructs = new LambdaConstructs(this, 'LambdaConstructs');
 
-		// example resource
-		// const queue = new sqs.Queue(this, 'InfraQueue', {
-		//   visibilityTimeout: cdk.Duration.seconds(300)
-		// });
+		s3Bucket.bucket.grantRead(lambdaConstructs.ingestionLambda);
+
+		s3Bucket.bucket.addEventNotification(EventType.OBJECT_CREATED, new s3n.LambdaDestination(lambdaConstructs.ingestionLambda), {
+			prefix: 'upload/',
+		});
 	}
 }
