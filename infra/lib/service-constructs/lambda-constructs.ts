4a35fc910d4fd2065472ce51eb3650fbf41db257
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-04-04 19:51:48 +0530

    feat: add LambdaConstructs for ingestion Lambda function setup

---
+++ infra/lib/service-constructs/lambda-constructs.ts
@@ -0,0 +1,18 @@
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Construct } from 'constructs';
+import * as path from 'path';
+
+export class LambdaConstructs extends Construct {
+	public readonly ingestionLambda: NodejsFunction;
+
+	constructor(scope: Construct, id: string) {
+		super(scope, id);
+
+		this.ingestionLambda = new NodejsFunction(this, 'IngestionLambda', {
+			runtime: Runtime.NODEJS_22_X,
+			entry: path.join(__dirname, '..', '..', '..', 'lambdas', 'ingestion', 'index.ts'),
+			handler: 'handler',
+		});
+	}
+}
