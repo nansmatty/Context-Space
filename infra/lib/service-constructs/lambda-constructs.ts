92b8ecff4581d02188b0627d0437050ba88005e1
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-04-05 22:12:05 +0530

    feat: switch PDF extraction from pdf-parse to unpdf for improved text processing

--- infra/lib/service-constructs/lambda-constructs.ts
+++ infra/lib/service-constructs/lambda-constructs.ts
@@ -10,29 +10,38 @@ export class LambdaConstructs extends Construct {
 	constructor(scope: Construct, id: string) {
 		super(scope, id);
 
+		// This lambda setup is configured with PDF Parser package.
+		// this.ingestionLambda = new NodejsFunction(this, 'IngestionLambda', {
+		// 	runtime: Runtime.NODEJS_22_X,
+		// 	entry: path.join(__dirname, '..', '..', '..', 'lambdas', 'src', 'ingestion-handler', 'index.ts'),
+		// 	handler: 'handler',
+		// 	depsLockFilePath: path.join(__dirname, '..', '..', '..', 'lambdas', 'package-lock.json'),
+		// 	projectRoot: path.join(__dirname, '..', '..', '..', 'lambdas'),
+		// 	timeout: Duration.seconds(30),
+		// 	bundling: {
+		// 		externalModules: ['@aws-sdk/*', '@smithy/*'],
+		// 		nodeModules: ['pdf-parse'],
+		// 		forceDockerBundling: true,
+		// 		commandHooks: {
+		// 			beforeBundling() {
+		// 				return [];
+		// 			},
+		// 			beforeInstall() {
+		// 				return [];
+		// 			},
+		// 			afterBundling(_inputDir: string, outputDir: string) {
+		// 				return [`rm -rf ${outputDir}/node_modules/.bin`];
+		// 			},
+		// 		},
+		// 	},
+		// });
+
+		// This lambda setup is configured with UnPDF package.
 		this.ingestionLambda = new NodejsFunction(this, 'IngestionLambda', {
 			runtime: Runtime.NODEJS_22_X,
 			entry: path.join(__dirname, '..', '..', '..', 'lambdas', 'src', 'ingestion-handler', 'index.ts'),
 			handler: 'handler',
-			depsLockFilePath: path.join(__dirname, '..', '..', '..', 'lambdas', 'package-lock.json'),
-			projectRoot: path.join(__dirname, '..', '..', '..', 'lambdas'),
 			timeout: Duration.seconds(30),
-			bundling: {
-				externalModules: ['@aws-sdk/*', '@smithy/*'],
-				nodeModules: ['pdf-parse'],
-				forceDockerBundling: true,
-				commandHooks: {
-					beforeBundling() {
-						return [];
-					},
-					beforeInstall() {
-						return [];
-					},
-					afterBundling(_inputDir: string, outputDir: string) {
-						return [`rm -rf ${outputDir}/node_modules/.bin`];
-					},
-				},
-			},
 		});
 	}
 }
--- lambdas/src/services/parser.service.ts
+++ lambdas/src/services/parser.service.ts
@@ -1,13 +1,29 @@
-import { CanvasFactory } from 'pdf-parse/worker';
-import { PDFParse } from 'pdf-parse';
+// import { CanvasFactory } from 'pdf-parse/worker';
+// import { PDFParse } from 'pdf-parse';
+import { extractText } from 'unpdf';
 export class ParserService {
+	// this version is for pdf-parser package.
+	// async extractTextFromPDF(buffer: Buffer): Promise<string> {
+	// 	const parser = new PDFParse({ data: buffer, CanvasFactory });
+	// 	try {
+	// 		const result = await parser.getText();
+	// 		return result.text?.trim() || '';
+	// 	} finally {
+	// 		await parser.destroy();
+	// 	}
+	// }
+
+	// this version is for unpdf package.
 	async extractTextFromPDF(buffer: Buffer): Promise<string> {
-		const parser = new PDFParse({ data: buffer, CanvasFactory });
 		try {
-			const result = await parser.getText();
-			return result.text?.trim() || '';
-		} finally {
-			await parser.destroy();
+			const { text } = await extractText(new Uint8Array(buffer), {
+				mergePages: true,
+			});
+
+			return text;
+		} catch (error) {
+			console.error('UNPDF: error extracting text', error);
+			throw error;
 		}
 	}
 
