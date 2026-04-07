3ff9cd62a9d2ed2bd0ca7d924b8c5acd37d21f50
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-04-07 19:43:54 +0530

    feat: add VPC and database outputs to ContextSpaceStack for enhanced infrastructure management

--- infra/lib/stack/infra-stack.ts
+++ infra/lib/stack/infra-stack.ts
@@ -4,16 +4,37 @@ import { S3BucketConstruct } from '../service-constructs/s3-bucket-construct';
 import { LambdaConstructs } from '../service-constructs/lambda-constructs';
 import { EventType } from 'aws-cdk-lib/aws-s3';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { DatabaseConstruct } from '../service-constructs/database-construct';
 
 export class ContextSpaceStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
+		const vpc = new ec2.Vpc(this, 'ContextSpaceVPC', {
+			maxAzs: 2,
+			natGateways: 1,
+		});
+
+		// Construct Calls
 		const s3Bucket = new S3BucketConstruct(this, 'S3BucketConstruct');
 		const lambdaConstructs = new LambdaConstructs(this, 'LambdaConstructs');
+		const database = new DatabaseConstruct(this, 'DatabaseConstruct', vpc);
 
+		new cdk.CfnOutput(this, 'DbSecretArn', {
+			value: database.cluster.secret!.secretArn,
+		});
+
+		new cdk.CfnOutput(this, 'DbEndpoint', {
+			value: database.cluster.clusterEndpoint.hostname,
+		});
+
+		new cdk.CfnOutput(this, 'DbPort', {
+			value: database.cluster.clusterEndpoint.port.toString(),
+		});
+
+		// Permission and Event Listeners
 		s3Bucket.bucket.grantRead(lambdaConstructs.ingestionLambda);
-
 		s3Bucket.bucket.addEventNotification(EventType.OBJECT_CREATED, new s3n.LambdaDestination(lambdaConstructs.ingestionLambda), {
 			prefix: 'upload/',
 		});
