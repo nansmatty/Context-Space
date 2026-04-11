4315a466655bc12e13f8dde719b563fdcb82f1f8
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-04-11 20:45:04 +0530

    feat: update runMigrations function to use correct migrations directory and add logging for found migration files

--- lambdas/src/migration-handler/index.ts
+++ lambdas/src/migration-handler/index.ts
@@ -1,5 +1,9 @@
+import { runMigrations } from '../db/run-migrations';
+
 export const handler = async () => {
 	console.log('Migration Lambda invoked');
+	await runMigrations();
+	console.log('Run migration function invoked');
 	return {
 		statusCode: 200,
 		body: JSON.stringify({ message: 'Migration Lambda is working' }),
