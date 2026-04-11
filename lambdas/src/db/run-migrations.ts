b399cfbde3be8f7cdd17ee640a5c6a21406a43a9
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-04-11 21:17:57 +0530

    fix: correct migrations directory path and clean up logging in runMigrations function

--- lambdas/src/db/run-migrations.ts
+++ lambdas/src/db/run-migrations.ts
@@ -7,7 +7,7 @@ export async function runMigrations() {
 	const dbClient = await createDbClient();
 
 	try {
-		const migrationsDir = path.join(__dirname, '..', 'db', 'migrations');
+		const migrationsDir = path.join(__dirname, 'db', 'migrations');
 		const files = await fs.readdir(migrationsDir);
 
 		const sqlFiles = files.filter((file) => file.endsWith('.sql')).sort((a, b) => a.localeCompare(b));
@@ -15,21 +15,18 @@ export async function runMigrations() {
 		if (sqlFiles.length === 0) {
 			console.log('No migrations files found');
 			return;
-		} else {
-			console.log('Migration files found:', files);
-			console.log('Migration files found2:', sqlFiles);
 		}
 
-		// for (const file of sqlFiles) {
-		// 	const filePath = path.join(migrationsDir, file);
-		// 	const sql = await fs.readFile(filePath, 'utf-8');
+		for (const file of sqlFiles) {
+			const filePath = path.join(migrationsDir, file);
+			const sql = await fs.readFile(filePath, 'utf-8');
 
-		// 	console.log(`Running migration: ${file}`);
-		// 	await dbClient.query(sql);
-		// 	console.log(`Finished migration: ${file}`);
-		// }
+			console.log(`Running migration: ${file}`);
+			await dbClient.query(sql);
+			console.log(`Finished migration: ${file}`);
+		}
 
-		// console.log('All migration completed successfully');
+		console.log('All migration completed successfully');
 	} finally {
 		await dbClient.end();
 	}
