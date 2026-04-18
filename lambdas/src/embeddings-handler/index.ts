394babe70c8b4aeb8645705d611a46680259ea6d
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-04-18 20:02:39 +0530

    feat: enhance SQS message handling and logging in embeddings handler

--- lambdas/src/embeddings-handler/index.ts
+++ lambdas/src/embeddings-handler/index.ts
@@ -1,17 +1,48 @@
+import { generateEmbeddings } from '../services/bedrock.service';
+import { DbInsertationPayload, EmbeddingsQueueEnvelope } from '../utils/shared_types';
+import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
+
+const sqs = new SQSClient({});
+
+const queueUrl = process.env.DATABASE_DATA_QUEUE_URL!;
+if (!queueUrl) {
+	throw new Error('DATABASE_DATA_QUEUE_URL is missing');
+}
+
 export const handler = async (event: any) => {
-	try {
-		console.log('Received SQS event:', JSON.stringify(event, null, 2));
+	for (const record of event.Records) {
+		try {
+			const message = JSON.parse(record.body) as EmbeddingsQueueEnvelope;
 
-		for (const record of event.Records) {
-			const envelope = JSON.parse(record.body);
+			if (message.type !== 'EMBEDDINGS_REQUEST') {
+				console.warn(`Skipping message with unsupported type: ${message.type}`);
+				continue;
+			}
 
-			console.log('Message type:', envelope.type);
-			console.log('Payload:', envelope.payload);
+			const { chunk_index, text } = message.payload;
+
+			if (Number.isNaN(chunk_index)) {
+				throw new Error(`Invalid chunk_index: ${chunk_index}`);
+			}
+
+			const embedding = await generateEmbeddings(text);
+
+			const dbPayload: DbInsertationPayload = {
+				payload: message.payload,
+				embedding: embedding,
+			};
+
+			const sendMessageCommand = new SendMessageCommand({
+				QueueUrl: queueUrl,
+				MessageBody: JSON.stringify(dbPayload),
+			});
+
+			await sqs.send(sendMessageCommand);
+
+			return { success: true };
+		} catch (error) {
+			console.error('Error processing SQS message:', error);
+			throw error;
 		}
-
-		return { success: true };
-	} catch (error) {
-		console.error('Error processing SQS message:', error);
-		throw error;
 	}
 };
