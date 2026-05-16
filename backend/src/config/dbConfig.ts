9a2e1abfd7b8bc29297d37077b25d7eb48939bc7
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-05-16 11:02:10 +0530

    feat: enhance global error handling with AppError class and asyncHandler utility

--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import { logger } from '../utils/logger';
 
 const MONGO_OPTIONS = {
--- backend/src/utils/global-error-handler.ts
+++ backend/src/utils/global-error-handler.ts
@@ -1,9 +1,19 @@
+import { NextFunction, Request, Response, RequestHandler } from 'express';
+
 export class AppError extends Error {
 	statusCode: number;
+	isOperational: boolean;
 
 	constructor(message: string, statusCode: number) {
 		super(message);
 		this.statusCode = statusCode;
+		this.isOperational = true;
 		Error.captureStackTrace(this, this.constructor);
 	}
 }
+
+export const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>): RequestHandler => {
+	return (req, res, next) => {
+		Promise.resolve(fn(req, res, next)).catch(next);
+	};
+};
