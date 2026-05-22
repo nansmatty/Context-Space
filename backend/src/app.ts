436dc74499b38848f813f40de0e36ef0f9ff51fb
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-05-22 13:07:20 +0530

    feat: add cookie-parser middleware and implement setAuthCookie utility for secure token handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import rateLimit from 'express-rate-limit';
 import mongoSanitize from 'express-mongo-sanitize';
 import hpp from 'hpp';
 import crypto from 'crypto';
+import cookieParser from 'cookie-parser';
 
 import { errorHandler } from './middlewares/error-middleware';
 import documentRoutes from './modules/document/document.routes';
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(helmet());
+app.use(cookieParser());
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
--- backend/src/utils/auth.utils.ts
+++ backend/src/utils/auth.utils.ts
@@ -1,5 +1,6 @@
 import jwt, { Secret, SignOptions } from 'jsonwebtoken';
 import { env } from '../config/env';
+import { Response } from 'express';
 
 export const generateAccessToken = (payload: Record<string, unknown>) => {
 	const secret: Secret = env.JWT_SECRET;
@@ -13,4 +14,13 @@ export const generateAccessToken = (payload: Record<string, unknown>) => {
 export const otpGeneration = (): string => {
 	const otp = Math.floor(100000 + Math.random() * 900000).toString();
 	return otp;
+};
+
+export const setAuthCookie = (res: Response, token: string) => {
+	res.cookie('accessToken', token, {
+		httpOnly: true,
+		secure: env.NODE_ENV === 'production',
+		sameSite: env.NODE_ENV === 'production' ? 'none' : 'lax',
+		maxAge: 7 * 24 * 60 * 60 * 1000,
+	});
 };
